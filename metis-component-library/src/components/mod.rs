b7507c3c8391a99baa295fe4a5fe9e29531366df
Metis Worker <metis-worker@example.com>
2026-01-27 06:19:26 +0000

    Add Select component wrapper

--- metis-component-library/src/components/mod.rs
+++ metis-component-library/src/components/mod.rs
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 pub mod input;
+pub mod select;
 
 pub use input::*;
+pub use select::{
+    Select, SelectGroup, SelectGroupLabel, SelectItemIndicator, SelectList, SelectOption,
+    SelectTrigger, SelectValue,
+};
