698582eef752969177113b4e0d1382ca433a15d3
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2026-01-27 16:23:30 -0800

    really fix the migrations (#857)

--- metis-server/migrations/20260127000000_add_versioned_rows.sql
+++ metis-server/migrations/20260127000000_add_versioned_rows.sql
@@ -9,8 +9,17 @@ ALTER TABLE metis.issues
     ALTER COLUMN version_number SET NOT NULL;
 ALTER TABLE metis.issues
     DROP CONSTRAINT IF EXISTS issues_pkey;
-ALTER TABLE metis.issues
-    ADD CONSTRAINT IF NOT EXISTS issues_id_version_unique UNIQUE (id, version_number);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint
+        WHERE conname = 'issues_id_version_unique'
+        AND conrelid = 'metis.issues'::regclass
+    ) THEN
+        ALTER TABLE metis.issues
+            ADD CONSTRAINT issues_id_version_unique UNIQUE (id, version_number);
+    END IF;
+END $$;
 CREATE INDEX IF NOT EXISTS issues_latest_idx
     ON metis.issues (id, version_number DESC);
 
@@ -24,8 +33,17 @@ ALTER TABLE metis.patches
     ALTER COLUMN version_number SET NOT NULL;
 ALTER TABLE metis.patches
     DROP CONSTRAINT IF EXISTS patches_pkey;
-ALTER TABLE metis.patches
-    ADD CONSTRAINT IF NOT EXISTS patches_id_version_unique UNIQUE (id, version_number);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint
+        WHERE conname = 'patches_id_version_unique'
+        AND conrelid = 'metis.patches'::regclass
+    ) THEN
+        ALTER TABLE metis.patches
+            ADD CONSTRAINT patches_id_version_unique UNIQUE (id, version_number);
+    END IF;
+END $$;
 CREATE INDEX IF NOT EXISTS patches_latest_idx
     ON metis.patches (id, version_number DESC);
 
@@ -39,8 +57,17 @@ ALTER TABLE metis.tasks
     ALTER COLUMN version_number SET NOT NULL;
 ALTER TABLE metis.tasks
     DROP CONSTRAINT IF EXISTS tasks_pkey;
-ALTER TABLE metis.tasks
-    ADD CONSTRAINT IF NOT EXISTS tasks_id_version_unique UNIQUE (id, version_number);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint
+        WHERE conname = 'tasks_id_version_unique'
+        AND conrelid = 'metis.tasks'::regclass
+    ) THEN
+        ALTER TABLE metis.tasks
+            ADD CONSTRAINT tasks_id_version_unique UNIQUE (id, version_number);
+    END IF;
+END $$;
 CREATE INDEX IF NOT EXISTS tasks_latest_idx
     ON metis.tasks (id, version_number DESC);
 
@@ -54,8 +81,17 @@ ALTER TABLE metis.task_status_logs
     ALTER COLUMN version_number SET NOT NULL;
 ALTER TABLE metis.task_status_logs
     DROP CONSTRAINT IF EXISTS task_status_logs_pkey;
-ALTER TABLE metis.task_status_logs
-    ADD CONSTRAINT IF NOT EXISTS task_status_logs_id_version_unique UNIQUE (id, version_number);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint
+        WHERE conname = 'task_status_logs_id_version_unique'
+        AND conrelid = 'metis.task_status_logs'::regclass
+    ) THEN
+        ALTER TABLE metis.task_status_logs
+            ADD CONSTRAINT task_status_logs_id_version_unique UNIQUE (id, version_number);
+    END IF;
+END $$;
 CREATE INDEX IF NOT EXISTS task_status_logs_latest_idx
     ON metis.task_status_logs (id, version_number DESC);
 
@@ -69,8 +105,17 @@ ALTER TABLE metis.users
     ALTER COLUMN version_number SET NOT NULL;
 ALTER TABLE metis.users
     DROP CONSTRAINT IF EXISTS users_pkey;
-ALTER TABLE metis.users
-    ADD CONSTRAINT IF NOT EXISTS users_id_version_unique UNIQUE (id, version_number);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint
+        WHERE conname = 'users_id_version_unique'
+        AND conrelid = 'metis.users'::regclass
+    ) THEN
+        ALTER TABLE metis.users
+            ADD CONSTRAINT users_id_version_unique UNIQUE (id, version_number);
+    END IF;
+END $$;
 CREATE INDEX IF NOT EXISTS users_latest_idx
     ON metis.users (id, version_number DESC);
 
@@ -84,8 +129,17 @@ ALTER TABLE metis.repositories
     ALTER COLUMN version_number SET NOT NULL;
 ALTER TABLE metis.repositories
     DROP CONSTRAINT IF EXISTS repositories_pkey;
-ALTER TABLE metis.repositories
-    ADD CONSTRAINT IF NOT EXISTS repositories_id_version_unique UNIQUE (id, version_number);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint
+        WHERE conname = 'repositories_id_version_unique'
+        AND conrelid = 'metis.repositories'::regclass
+    ) THEN
+        ALTER TABLE metis.repositories
+            ADD CONSTRAINT repositories_id_version_unique UNIQUE (id, version_number);
+    END IF;
+END $$;
 CREATE INDEX IF NOT EXISTS repositories_latest_idx
     ON metis.repositories (id, version_number DESC);
 
@@ -99,7 +153,16 @@ ALTER TABLE metis.actors
     ALTER COLUMN version_number SET NOT NULL;
 ALTER TABLE metis.actors
     DROP CONSTRAINT IF EXISTS actors_pkey;
-ALTER TABLE metis.actors
-    ADD CONSTRAINT IF NOT EXISTS actors_id_version_unique UNIQUE (id, version_number);
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_constraint
+        WHERE conname = 'actors_id_version_unique'
+        AND conrelid = 'metis.actors'::regclass
+    ) THEN
+        ALTER TABLE metis.actors
+            ADD CONSTRAINT actors_id_version_unique UNIQUE (id, version_number);
+    END IF;
+END $$;
 CREATE INDEX IF NOT EXISTS actors_latest_idx
     ON metis.actors (id, version_number DESC);
