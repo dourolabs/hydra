840a0471be963326f4929b57b165cb98d19527df
Metis Worker <metis-worker@example.com>
2026-01-29 23:24:43 +0000

    Add toggle switch component

--- metis-component-library/src/lib.rs
+++ metis-component-library/src/lib.rs
@@ -2,5 +2,5 @@ pub mod components;
 
 pub use components::{
     Button, ButtonSize, ButtonVariant, Input, Select, SelectGroup, SelectGroupLabel,
-    SelectItemIndicator, SelectList, SelectOption, SelectTrigger, SelectValue,
+    SelectItemIndicator, SelectList, SelectOption, SelectTrigger, SelectValue, ToggleSwitch,
 };
