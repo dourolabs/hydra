01c68c475b698cc2ef56b5c8930913efe0a1c75a
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2026-03-19 07:45:13 -0700

    Revert "fix migrations (#2286)"
    This reverts commit f541dfc62bfb62a446357b64803ab43ff5b20a98.

--- hydra-server/migrations/20260326000000_rename_schema_metis_to_hydra.sql
+++
@@ -1,7 +0,0 @@
--- Rename the PostgreSQL schema from metis to hydra as part of the
--- project-wide rename. This must be applied after all prior migrations
--- (which created objects under the metis schema) and will make the
--- schema name match the new hydra crate and code references.
-
--- we don't run this anymore because after renaming metis -> hydra, it confused sqlx migrations
--- ALTER SCHEMA metis RENAME TO hydra;
